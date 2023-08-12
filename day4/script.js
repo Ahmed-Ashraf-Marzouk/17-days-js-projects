b97f6e5d2eafd558a8bb2ed985c8998114d3cafc
Ahmed-Ashraf-Marzouk <a.a.elghawas@gmail.com>
2023-08-12 18:34:46 +0300

    updated day4

--- day4/script.js
+++ day4/script.js
@@ -6,6 +6,8 @@ const searchIcon = document.querySelector('.search-icon')
 searchIcon.addEventListener('click', () => {
     searchContainer.classList.toggle('active')
     textField.focus()
+    
+    // To put the cursor at the end of the text 
     var val = textField.value; //store the value of the element
     textField.value = ''; //clear the value of the element
     textField.value = val; //set that value back.  
