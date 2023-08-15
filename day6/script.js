e9925fce5d7588db70522491e9cd0ce7c867ab90
Ahmed-Ashraf-Marzouk <a.a.elghawas@gmail.com>
2023-08-15 18:10:39 +0300

    Finished day7

--- day6/script.js
+++ day6/script.js
@@ -1,11 +1,11 @@
-const divs = document.querySelectorAll('div')
-
+const divs = document.querySelectorAll('.rect')
+const arrow = document.querySelector('.arrow')
 
 yAxis = 0
 
 
 window.onscroll = () => {
-    const triggerBottom = window.innerHeight / 5 * 4
+    const triggerBottom = window.innerHeight / 5 * 4 /* Why these numbers? */
     for(let i = 0; i < divs.length; i++){
             const box = divs[i].getBoundingClientRect().top
             if(triggerBottom > box){
@@ -17,3 +17,25 @@ window.onscroll = () => {
     }
 }
 
+
+arrow.addEventListener('click', pageScroll)
+
+
+// Why it lags sometimes? 
+function pageScroll() {
+    window.scrollBy(0, 10); // horizontal and vertical scroll increments
+    scrolldelay = setTimeout('pageScroll()', 20); // scrolls every 20 milliseconds
+    if ((window.innerHeight + window.offsetHeight) >= document.body.offsetHeight) {
+      clearTimeout(scrolldelay);
+      console.log("Ahmed Ali")
+    //   scrolldelay = setTimeout('PageUp()', 30);
+    }
+  
+  }
+  
+//   function PageUp() {
+//     window.scrollTo(0, 0);
+//     setTimeout(function() {
+//       pageScroll()
+//     }, 600);
+//   }
